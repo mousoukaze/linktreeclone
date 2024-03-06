071d1f9eb9bf9ae2b368decbda2548d06f57eab3
Delusion <mousoukaze@gmail.com>
2024-03-07 01:28:07 +0700

    navbar added / gotta learn more css (copied from provided source code)

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 end
